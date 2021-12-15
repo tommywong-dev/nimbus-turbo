92a68daa91ce93b34da8111dffa397fe29de8a86
Tommy Wong <tommywong.dev@gmail.com>
2021-12-15 09:18:00 +0800

    removed something

--- apps/server/main.go
+++ apps/server/main.go
@@ -8,7 +8,6 @@ func main() {
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
 
-	router.SetTrustedProxies([]string{"192.168.0.112"})
 	// Serve frontend static files
 	router.Static("/", "../web/build")
 
